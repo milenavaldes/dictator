7cba2c8136f568de23e7f7f61e711cb61ab21e9c
Milena Valdes <milenavaldes1@gmail.com>
2024-05-11 20:04:21 -0400

    set up speach module

---
+++ src/utils/useTTS.ts
@@ -0,0 +1,28 @@
+import Tts from 'react-native-tts';
+
+const initializeTTS = () => {
+    Tts.setDefaultLanguage('en-US');
+    Tts.setDefaultRate(0.5);
+    Tts.setDefaultPitch(1.0);
+  
+    Tts.addEventListener('tts-start', event => console.log("start", event));
+    Tts.addEventListener('tts-finish', event => console.log("finish", event));
+    Tts.addEventListener('tts-error', event => console.error("error", event));
+  };
+
+  
+  const speak = (text: string) => {
+    Tts.speak(text);
+  };
+  
+  const stop = () => {
+    Tts.stop();
+  };
+  
+
+  export default {
+    initializeTTS,
+    speak,
+    stop
+  };
+  
