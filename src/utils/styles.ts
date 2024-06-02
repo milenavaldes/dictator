cb6f672500c8d3036db32b32c4230150889859f0
Milena Valdes <milenavaldes1@gmail.com>
2024-06-02 13:09:40 -0400

    privacy file change

---
+++ src/utils/styles.ts
@@ -0,0 +1,41 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  headline: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  textInput: {
+    minHeight: 100,
+    borderWidth: 1,
+    borderColor: 'grey',
+    marginBottom: 20,
+    padding: 10,
+  },
+  list: {
+    flexGrow: 0,
+    marginBottom: 20,
+  },
+  listItem: {
+    fontSize: 16,
+    padding: 10,
+  },
+  scrollContainer: {},
+  dictatePage: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around', // Распределяет кнопки равномерно
+    marginBottom: 20,
+  },
+});
+
+export default styles;
